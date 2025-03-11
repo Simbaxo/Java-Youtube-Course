39f4a1c539e4f1b670873b7cd79a9d382bf12727
Simbaxo <ballerlikemike23@gmail.com>
2025-03-11 14:55:05 -0500

    Intro

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Primitive vs Reference
+        // ---------    ---------
+        // int          string
+        // double       array
+        // char         object
+        // boolean
+
+        // 2 Steps to creating a variable
+        // -----------------------------
+        // 1. declaration
+        // 2. assignment
+
+        int age =21;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double tempature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnLine = true;
+
+        String name = "Andres Mena";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+
+
+
+    }
+}
