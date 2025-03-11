14ec7a55652b9c0d940960fba665cd2642e98c39
Simbaxo <ballerlikemike23@gmail.com>
2025-03-11 15:42:38 -0500

    Lunch Break

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -2,31 +2,47 @@ import java.util.Scanner;
 
 public class DayTwo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 
         // COMMON ISSUES
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.print("What is your gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("Are you a student? (true/false): ");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("Your are " + age + " years old.");
+//        System.out.println("Your gpa is " + gpa);
+//
+//        if(isStudent){
+//            System.out.println("You are enrolled as a student.");
+//        } else {
+//            System.out.println("You are not enrolled");
+//        }
+//
+//        scanner.close();
 
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        // Calculate area of a rectange
 
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
+        double width = 0;
+        double height = 0;
+        double area = 0;
 
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello " + name);
-        System.out.println("Your are " + age + " years old.");
-        System.out.println("Your gpa is " + gpa);
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
 
-        if(isStudent){
-            System.out.println("You are enrolled as a student.");
-        } else {
-            System.out.println("You are not enrolled");
-        }
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
 
         scanner.close();
     }
