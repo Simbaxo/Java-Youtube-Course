f47685c543b93ab2caeba065265546aa62b87f7b
Simbaxo <ballerlikemike23@gmail.com>
2025-03-22 19:46:39 -0500

    Dinner Break

---
+++ src/DayFour.java
@@ -0,0 +1,30 @@
+public class DayFour {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+//        int z;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+
+//        System.out.println(z);
+
+        // Augmented Assignment Operators
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+
+        // Increment and Decrement Operators
+
+        System.out.println(x);
+    }
+}
